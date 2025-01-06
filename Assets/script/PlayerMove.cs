39eb31520555071595cf1b2135d45a00eb7bdc4f
dimensi4 <lmarnette0@gmail.com>
2025-01-06 21:47:07 +0100

    Initial commit

---
+++ Assets/script/PlayerMove.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+    public float Vitesse = 5f;
+    Vector2 mouvement;
+    public Animator animator;
+
+    void Update()
+    {
+        mouvement.x = Input.GetAxisRaw ("Horizontal");
+        mouvement.y = Input.GetAxisRaw ("Vertical");
+
+        animator.SetFloat("Horizontal", mouvement.x);
+        animator.SetFloat("Speed",mouvement.magnitude);
+
+        rb.MovePosition(rb.position + mouvement * Vitesse * Time.deltaTime);
+    }
+}
